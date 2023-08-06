3696d4aa4df46bd166253e836b497399c599055b
forkanimahdi <forkanimahdi@gmail.com>
2023-08-06 13:01:17 +0100

    Updates

--- src/layouts/Footer.js
+++
@@ -1,9 +0,0 @@
-export const FooterComponent = () => {
-    return (
-        <>
-        <div className="footer">
-        <h3> &copy;  Powred by Forkani Mehdi</h3>
-        </div>
-        </>
-    )
-}
